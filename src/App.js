d44b383699e9849bb9f3996cb129054389fb6d2f
salmanshah1d <s.shahid@mail.utoronto.ca>
2020-11-28 18:35:55 -0500

    Put placeholder website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="intro">
+      <p>Hi, my name is Salman, and I am interested in AI research, product management, and entrepreneurship. This website is a work in progress, but in the meantime, please check out my <a target='_blank' rel="noreferrer" href='https://github.com/salmanshah1d'>resume</a>, <a target='_blank' rel="noreferrer" href='https://linkedin.com/in/salman-shahid'>LinkedIn</a>.</p>
+      <p>Please feel free to reach out to me at <a href="mailto:s.shahid@mail.utoronto.ca">s.shahid@mail.utoronto.ca</a>.</p>
     </div>
   );
 }
