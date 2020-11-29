e6d245776e9c8432a375788420f612e09de9330c
salmanshah1d <s.shahid@mail.utoronto.ca>
2020-11-29 17:59:24 -0500

    added github link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="intro">
-      <p>Hi, my name is Salman, and I am interested in AI research, product management, and entrepreneurship. This website is a work in progress, but in the meantime, please check out my <a target='_blank' rel="noreferrer" href='https://github.com/salmanshah1d'>resume</a>, <a target='_blank' rel="noreferrer" href='https://linkedin.com/in/salman-shahid'>LinkedIn</a>.</p>
+      <p>Hi, my name is Salman, and I am interested in AI research, product management, and entrepreneurship. This website is a work in progress, but in the meantime, please check out my <a target='_blank' rel="noreferrer" href='https://drive.google.com/file/d/1SMWFI5QPZSy0TUeC1f8iuGu5oxaMR9pp/view'>resume</a>, <a target='_blank' rel="noreferrer" href='https://github.com/salmanshah1d'>GitHub</a>, <a target='_blank' rel="noreferrer" href='https://linkedin.com/in/salman-shahid'>LinkedIn</a>.</p>
       <p>Please feel free to reach out to me at <a href="mailto:s.shahid@mail.utoronto.ca">s.shahid@mail.utoronto.ca</a>.</p>
     </div>
   );
